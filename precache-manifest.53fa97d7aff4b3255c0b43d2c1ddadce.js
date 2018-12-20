1e310a81d85267b5faddb017db80afb009b0a49d
DChang <chang19d@gmail.com>
2018-12-19 19:40:18 -0500

    Updates

---
+++ precache-manifest.53fa97d7aff4b3255c0b43d2c1ddadce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "4deb857ef4945ce20f94",
+    "url": "/clickygame/static/js/main.4deb857e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/clickygame/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "4deb857ef4945ce20f94",
+    "url": "/clickygame/static/css/main.837678d5.chunk.css"
+  },
+  {
+    "revision": "c419d47ad981a4cf82e0cf6c57baa457",
+    "url": "/clickygame/index.html"
+  }
+];
